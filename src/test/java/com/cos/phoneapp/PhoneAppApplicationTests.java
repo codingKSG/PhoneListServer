5dfaba8fa94c7ddc8764c177d205d858bb6ed3c8
김상길 <soppwy3@gmail.com>
2021-03-08 22:23:53 +0900

    연락처 서버

---
+++ src/test/java/com/cos/phoneapp/PhoneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.phoneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
